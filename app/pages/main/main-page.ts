99a2d00c05504cd0fc9c01165aa9c5cc0f212937
matiasratcliffe <mati.ratcliffe@gmail.com>
2018-04-08 20:48:36 -0300

    First view

---
+++ app/pages/main/main-page.ts
@@ -0,0 +1,13 @@
+import {EventData, Observable} from "data/observable";
+
+class StartPageController extends Observable {
+
+  public pageLoaded(args: EventData) {
+    var page = <Page>args.object;
+    page.bindingContext = this;
+  }
+
+}
+
+var spc = new StartPageController();
+export var pageLoaded = args => spc.pageLoaded(args);
