b7e09b58167ed212227959a2286d0a2b9a7c424c
matiasratcliffe <mati.ratcliffe@gmail.com>
2018-04-22 17:30:45 -0300

    further testing

--- platforms/android/app/build/intermediates/assets/debug/app/pages/login/login.component.ts
+++ platforms/android/app/build/intermediates/assets/debug/app/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit} from "@angular/core";
 import { BaseService } from "../../services/base.service";
 import { Page } from "tns-core-modules/ui/page";
 
+import { User } from "../../model/user.model";
+
 @Component({
     selector: "app-login",
 		templateUrl: "pages/login/login.component.html",
@@ -10,9 +12,33 @@ import { Page } from "tns-core-modules/ui/page";
 })
 export class LoginComponent implements OnInit {
 
-		constructor(private appService: BaseService, private page: Page) { }
+    user: User;
+		isLoggingIn = true;
+
+		constructor(private appService: BaseService, private page: Page) {
+			this.user = new User();
+		}
 
     ngOnInit() {
 			this.page.actionBarHidden = true;
     }
+
+		submit() {
+			 if (this.isLoggingIn) {
+					 this.login();
+			 } else {
+					 this.signUp();
+			 }
+	 }
+
+	 login() {
+	 }
+
+	 signUp() {
+
+	 }
+
+	 toggleDisplay() {
+			 this.isLoggingIn = !this.isLoggingIn;
+	 }
 }
