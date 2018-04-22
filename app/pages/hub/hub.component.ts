af813c83568332f36ab164cf60e72954f4c61d11
Ignacio Delmont <ignaciodelmont@gmail.com>
2018-04-22 19:00:40 -0300

    added hub components

---
+++ app/pages/hub/hub.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit} from "@angular/core";
+import { BaseService } from "../../services/base.service";
+import { Page } from "tns-core-modules/ui/page";
+
+import { User } from "../../model/user.model";
+
+@Component({
+    selector: "app-hub",
+		templateUrl: "pages/hub/hub.component.html",
+		styleUrls: ["pages/hub/hub.component.css"],
+		providers: [BaseService]
+})
+export class HubComponent implements OnInit {
+
+    user: User;
+
+		constructor(private appService: BaseService, private page: Page) {
+			this.user = new User();
+		}
+
+    ngOnInit() {
+			this.page.actionBarHidden = true;
+    }
+
+}
