294d60e6b290a0d9df67943547b6f76f2c79df43
matiasratcliffe <mati.ratcliffe@gmail.com>
2018-04-23 19:50:30 -0300

    merged master to BasicInterface

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,12 +1,15 @@
 import { StartComponent } from "./pages/start/start.component";
 import { LoginComponent } from "./pages/login/login.component";
+import { HubComponent } from "./pages/hub/hub.component";
 
 export const routes = [
     { path: "", component: StartComponent },
-		{ path: "login", component: LoginComponent }
+		{ path: "login", component: LoginComponent },
+    { path: "hub", component: HubComponent }
 ];
 
 export const navigatableComponents = [
     StartComponent,
-		LoginComponent
+		LoginComponent,
+    HubComponent
 ];
