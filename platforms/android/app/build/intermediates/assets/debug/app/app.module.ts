af81e7968712cc5504d5a1c7a22af9f154efbcf7
matiasratcliffe <mati.ratcliffe@gmail.com>
2018-04-23 12:49:37 -0300

    updated package files

--- platforms/android/app/build/intermediates/assets/debug/app/app.module.ts
+++ platforms/android/app/build/intermediates/assets/debug/app/app.module.ts
@@ -1,25 +1,39 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
-import { NativeScriptHttpModule } from "nativescript-angular/http";
-import { NativeScriptFormsModule } from "nativescript-angular/forms";
-
+import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
-import { routes, navigatableComponents } from "./app.routing";
+
+import { ItemService } from "./item/item.service";
+import { ItemsComponent } from "./item/items.component";
+import { ItemDetailComponent } from "./item/item-detail.component";
+
+// Uncomment and add to NgModule imports if you need to use two-way binding
+// import { NativeScriptFormsModule } from "nativescript-angular/forms";
+
+// Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
+// import { NativeScriptHttpModule } from "nativescript-angular/http";
 
 @NgModule({
+    bootstrap: [
+        AppComponent
+    ],
     imports: [
         NativeScriptModule,
-				NativeScriptHttpModule,
-				NativeScriptFormsModule,
-				NativeScriptRouterModule,
-				NativeScriptRouterModule.forRoot(routes)
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
-				...navigatableComponents
+        ItemsComponent,
+        ItemDetailComponent
     ],
-		bootstrap: [AppComponent],
-    schemas: [NO_ERRORS_SCHEMA]
+    providers: [
+        ItemService
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
 })
+/*
+Pass your application module to the bootstrapModule function located in main.ts to start your app
+*/
 export class AppModule { }
