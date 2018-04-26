3435c4cee54d3942e61a0304682be85fe7c3008b
matiasratcliffe <mati.ratcliffe@gmail.com>
2018-04-26 09:38:16 -0300

    Deleted platforms (now ignored)

--- platforms/android/app/build/intermediates/assets/debug/app/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformNativeScriptDynamic } from "nativescript-angular/platform";
-import { AppModule } from "./app.module";
-
-import { enableProdMode } from "@angular/core";
-
-//enableProdMode();
-platformNativeScriptDynamic().bootstrapModule(AppModule);
