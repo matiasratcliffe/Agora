259610b2ad7409aff164c7f33f0fb8978e792d82
matiasratcliffe <mati.ratcliffe@gmail.com>
2018-04-27 20:41:17 -0300

    http post test

---
+++ app/model/app.config.ts
@@ -0,0 +1,3 @@
+export module AppConfig {
+    server: const string = "";
+}
--- app/services/base.service.ts
+++ app/services/base.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class BaseService {
+
 	pageStack: Array<Object> = [];
 
-	constructor (private router: Router) {}
+	constructor(private router: Router) { }
 
 	goto(location) {
 		this.router.navigate([location]);
