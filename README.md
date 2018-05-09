d6aa37f3ba1533f6b14818b6baa594ffbacdf8d6
mati <mati.ratcliffe@gmail.com>
2018-05-09 17:39:09 -0300

    ultimate

--- README.md
+++
@@ -1,2 +0,0 @@
-# Agora
-Politica no politizada
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,27 +1,25 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-//import { NativeScriptRouterModule } from "nativescript-angular/router";
-//import { NativeScriptHttpModule } from "nativescript-angular/http";
-//import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
-//import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 import { AppComponent } from "./app.component";
-//import { routes, navigatableComponents } from "./app.routing";
+import { routes, navigatableComponents } from "./app.routing";
 
 @NgModule({
 	imports: [
 		NativeScriptModule,
-		//NativeScriptHttpModule,
-		//NativeScriptHttpClientModule,
-		//NativeScriptFormsModule,
-		//NativeScriptRouterModule,
-		//NativeScriptRouterModule.forRoot(routes)
+		NativeScriptHttpClientModule,
+		NativeScriptFormsModule,
+		NativeScriptRouterModule,
+		NativeScriptRouterModule.forRoot(routes)
 	],
 	declarations: [
 		AppComponent,
-		//...navigatableComponents
+		...navigatableComponents
 	],
 	bootstrap: [AppComponent],
-	//schemas: [NO_ERRORS_SCHEMA]
+	schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
